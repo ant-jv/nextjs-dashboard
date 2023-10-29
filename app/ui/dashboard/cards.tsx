9d3ec695f89c5f2803573a845e4ed072d7fe72fa
Antti Viljanen <viljasenantti@gmail.com>
2023-10-29 13:35:58 +0200

    Chapter 9 completed

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,12 +14,13 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function Cards(
-  totalPaidInvoices: string,
-  totalPendingInvoices: string,
-  numberOfInvoices: number,
-  numberOfCustomers: number
-) {
+export default async function Cards() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
